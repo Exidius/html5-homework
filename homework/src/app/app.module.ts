88e2104cb3599a01cfc6ce52392658ed8b07889a
baradb <barad95@gmail.com>
2019-10-19 15:49:46 +0200

    new attempt

--- homework/src/app/app.module.ts
+++ homework/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 import { SpeciesComponent } from './species/species.component';
+import { SpecieComponent } from './species/specie/specie.component';
+import { SpecieListComponent } from './species/specie-list/specie-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    SpeciesComponent
+    HeroComponent,
+    HeroListComponent,
+    SpeciesComponent,
+    SpecieComponent,
+    SpecieListComponent
   ],
   imports: [
     BrowserModule,
